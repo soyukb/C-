8d909830319889764fd003128fec0226e00c7517
soyu <soyukoba@gmail.com>
2025-01-09 15:28:12 +0900

    first commit

---
+++ sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath> 
+
+using namespace std;
+int main(void){
+    int A,B;
+    cin >> A >> B;
+    cout << (A+B)*(A+B) <<endl;
+
+}
