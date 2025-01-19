4d5af99bcb3b58e07a83ac1450f986743bce7379
soyu <soyukoba@gmail.com>
2025-01-19 12:50:46 +0900

    first commit

--- sample.cpp
+++ sample.cpp
@@ -7,15 +7,6 @@
 using namespace std;
 
 int main() {
-    string s;
-    getline(cin,s);
-    int now=0;
-    for(int i=0;i<s.size();i++){
-        if(s[i]==' '){
-            cout << s.substr(now,i-now) << endl;
-            now+=i+1;
-        }
-       
-    }
-}
     
+
+}    
